98a33ac5b84ce21b3e50c87b478da01e91f94850
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2020-03-03 12:11:12 +0100

    Creacion del proyecto inicial

---
+++ sdi1920-entrega1-411-408/src/test/java/com/uniovi/Sdi1920Entrega1411408ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920Entrega1411408ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
