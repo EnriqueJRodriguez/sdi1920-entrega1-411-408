21c6e475cc284425d9a4cb53afbebb51cdb59ff2
fonsecadh <UO258318@uniovi.es>
2020-03-07 20:12:33 +0100

    Roles y validadores.

--- src/main/java/com/uniovi/controllers/UsersController.java
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -10,17 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.User;
+import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
 
 	@Autowired
+	private RolesService rolesService;
+
+	@Autowired
 	private UsersService usersService;
-	
+
 	@Autowired
 	private SecurityService securityService;
+
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
 
 	@RequestMapping(value = "/user/add")
 	public String getUser(Model model) {
@@ -42,11 +50,11 @@ public class UsersController {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(@Validated User user, BindingResult result) {
-//		signUpFormValidator.validate(user, result);
-//		if(result.hasErrors()) {
-//			return "signup";
-//		}
-//		user.setRole(rolesService.getRoles()[0]);
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
+		user.setRole(rolesService.getRoles()[0]);
 		usersService.addUser(user);
 		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
 		return "redirect:home";
