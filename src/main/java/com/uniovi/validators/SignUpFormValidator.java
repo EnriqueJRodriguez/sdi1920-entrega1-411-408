f7342134eab41abcad96881f713efd95b0fc8148
fonsecadh <UO258318@uniovi.es>
2020-03-08 18:55:13 +0100

    Usuarios: logearse y desconectarse. Validador de registro funciona.

--- src/main/java/com/uniovi/validators/SignUpFormValidator.java
+++ src/main/java/com/uniovi/validators/SignUpFormValidator.java
@@ -24,18 +24,11 @@ public class SignUpFormValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "Error.empty");
+			
+		if (usersService.getUserByEmail(user.getEmail()) != null) {
+			errors.rejectValue("email", "Error.signup.email.duplicate");
+		}
 		
-		// TODO: Implement this validator
-		
-		if (user.getName().length() < 5 || user.getName().length() > 24) {
-			errors.rejectValue("name", "Error.signup.name.length");
-		}
-		if (user.getLastName().length() < 5 || user.getLastName().length() > 24) {
-			errors.rejectValue("lastName", "Error.signup.lastName.length");
-		}
-		if (user.getPassword().length() < 5 || user.getPassword().length() > 24) {
-			errors.rejectValue("password", "Error.signup.password.length");
-		}
 		if (!user.getPasswordConfirm().equals(user.getPassword())) {
 			errors.rejectValue("passwordConfirm", "Error.signup.passwordConfirm.coincidence");
 		}
