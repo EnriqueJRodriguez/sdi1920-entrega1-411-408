4a833dfaa4f9684d9b8442e1ad5100a6ed7a3885
fonsecadh <UO258318@uniovi.es>
2020-03-12 19:40:57 +0100

    Invitaciones de amistad - En progreso.

--- src/main/java/com/uniovi/controllers/UsersController.java
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -71,5 +72,11 @@ public class UsersController {
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
 	public String home(Model model) {
 		return "home";
+	}
+	
+	@RequestMapping(value = "/user/{id}/invitation", method = RequestMethod.GET)
+	public String setResendTrue(Model model, @PathVariable Long id) {
+		usersService.setUserInvitation(true, id);
+		return "redirect:/user/list";
 	}
 }
