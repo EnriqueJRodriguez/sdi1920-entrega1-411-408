0cb00ee0588295278475bc79a8f8e05dfd246e88
EnriqueJRodriguez <enriquejrodriguezmartin@gmail.com>
2020-03-15 22:53:09 +0100

    Tests revisados para entrega

--- src/test/java/com/uniovi/tests/Sdi1920Entrega1411408ApplicationTests.java
+++ src/test/java/com/uniovi/tests/Sdi1920Entrega1411408ApplicationTests.java
@@ -192,7 +192,7 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Comprobamos que estamos en la pantalla de inicio
 		PO_LoginView.checkKey(driver, "login.title", PO_Properties.getSPANISH());
 		// Comprobamos que no podemos acceder a la pestaña de usuarios
-		PO_View.checkNoElement(driver, "users.title", PO_Properties.getSPANISH());
+		PO_NavView.checkNoElement(driver, "users.title", PO_Properties.getSPANISH());
 	}
 
 	// PR10. Comprobar que el botón cerrar sesión no está visible si el usuario no
@@ -211,13 +211,13 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Rellenamos el formulario
 		PO_LoginView.fillForm(driver, "jaimitosdi@uniovi.es", "123456");
 		// Comprobamos que aparecen usurios normales
-		PO_View.checkElement(driver, "text", "max@arcadia.com");
-		PO_View.checkElement(driver, "text", "pepitosdi@uniovi.es");
-		PO_View.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
+		PO_PrivateView.checkElement(driver, "text", "max@arcadia.com");
+		PO_PrivateView.checkElement(driver, "text", "pepitosdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
 		// Comprobamos que no sale el administrador
-		PO_View.checkNoElement(driver, "juansdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "juansdi@uniovi.es");
 		// Comprobamos que no sale el propio usuario logueado
-		PO_View.checkNoElement(driver, "jaimitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "jaimitosdi@uniovi.es");
 
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
@@ -235,13 +235,13 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Realizamos la busqueda
 		PO_PrivateView.search(driver, "");
 		// Comprobamos que aparecen usurios normales
-		PO_View.checkElement(driver, "text", "max@arcadia.com");
-		PO_View.checkElement(driver, "text", "pepitosdi@uniovi.es");
-		PO_View.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
+		PO_PrivateView.checkElement(driver, "text", "max@arcadia.com");
+		PO_PrivateView.checkElement(driver, "text", "pepitosdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
 		// Comprobamos que no sale el administrador
-		PO_View.checkNoElement(driver, "juansdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "juansdi@uniovi.es");
 		// Comprobamos que no sale el propio usuario logueado
-		PO_View.checkNoElement(driver, "jaimitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "jaimitosdi@uniovi.es");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
 	}
@@ -258,11 +258,11 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Realizamos la busqueda
 		PO_PrivateView.search(driver, "---------------------------------");
 		// Comprobamos que no aparecen usurios
-		PO_View.checkNoElement(driver, "max@arcadia.com");
-		PO_View.checkNoElement(driver, "pepitosdi@uniovi.es");
-		PO_View.checkNoElement(driver, "laurapalmer@twinpeaks.com");
-		PO_View.checkNoElement(driver, "juansdi@uniovi.es");
-		PO_View.checkNoElement(driver, "jaimitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "max@arcadia.com");
+		PO_PrivateView.checkNoElement(driver, "pepitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "laurapalmer@twinpeaks.com");
+		PO_PrivateView.checkNoElement(driver, "juansdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "jaimitosdi@uniovi.es");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
 	}
@@ -280,16 +280,16 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Realizamos la busqueda
 		PO_PrivateView.search(driver, "laurapalmer@twinpeaks.com");
 		// Comprobamos que solo se carga una pagina
-		PO_View.checkKey(driver, "pagination.first", PO_Properties.getSPANISH());
-		PO_View.checkElement(driver, "text", "1");
-		PO_View.checkKey(driver, "pagination.last", PO_Properties.getSPANISH());
+		PO_PrivateView.checkKey(driver, "pagination.first", PO_Properties.getSPANISH());
+		PO_PrivateView.checkElement(driver, "text", "1");
+		PO_PrivateView.checkKey(driver, "pagination.last", PO_Properties.getSPANISH());
 		// comprobamos que solo aparece el usuario buscado
-		PO_View.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
+		PO_PrivateView.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
 		// Comprobamos que no aparecen el resto usurios
-		PO_View.checkNoElement(driver, "max@arcadia.com");
-		PO_View.checkNoElement(driver, "pepitosdi@uniovi.es");
-		PO_View.checkNoElement(driver, "juansdi@uniovi.es");
-		PO_View.checkNoElement(driver, "jaimitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "max@arcadia.com");
+		PO_PrivateView.checkNoElement(driver, "pepitosdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "juansdi@uniovi.es");
+		PO_PrivateView.checkNoElement(driver, "jaimitosdi@uniovi.es");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
 	}
@@ -306,7 +306,7 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		PO_NavView.checkElement(driver, "text",
 				PO_NavView.getP().getString("nav.message.users", PO_Properties.getSPANISH()));
 		// Mandamos una invitatión al usuario jaimito
-		List<WebElement> botones = PO_View.checkElement(driver, "free",
+		List<WebElement> botones = PO_PrivateView.checkElement(driver, "free",
 				"/html/body/div[1]/div[1]/table/tbody/tr[td[contains(text(), 'jaimitosdi@uniovi.es')]]/td[4]/div/button");
 		botones.get(0).click();
 		// Nos desconectamos
@@ -316,13 +316,13 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Rellenamos el formulario
 		PO_LoginView.fillForm(driver, "jaimitosdi@uniovi.es", "123456");
 		// Pinchamos en la opción de menú de invitaciones
-		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
+		List<WebElement> elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de invitaciones.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
 		elementos.get(0).click();
 		// Comprobamos que tenemos una invitación de amistad
-		PO_NavView.checkElement(driver, "text", "pepitosdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "pepitosdi@uniovi.es");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
 	}
@@ -339,7 +339,7 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		PO_NavView.checkElement(driver, "text",
 				PO_NavView.getP().getString("nav.message.users", PO_Properties.getSPANISH()));
 		// Mandamos una invitatión a la usuario Max
-		List<WebElement> botones = PO_View.checkElement(driver, "free",
+		List<WebElement> botones = PO_PrivateView.checkElement(driver, "free",
 				"/html/body/div[1]/div[1]/table/tbody/tr[td[contains(text(), 'max@arcadia.com')]]/td[4]");
 		// Comprobamos que el botón no se muestra ya que existe una invitación
 		// entre éstos dos usuarios
@@ -359,13 +359,13 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		PO_NavView.checkElement(driver, "text",
 				PO_NavView.getP().getString("nav.message.users", PO_Properties.getSPANISH()));
 		// Pinchamos en la opción de menú de invitaciones
-		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
+		List<WebElement> elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de invitaciones.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
 		elementos.get(0).click();
 		// Contamos las invitaciones (Debería haber una de Max y otra de Laura)
-		elementos = PO_View.checkElement(driver, "free", "//td/div");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//td/div");
 		assertEquals(2, elementos.size());
 	}
 
@@ -382,17 +382,17 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		PO_NavView.checkElement(driver, "text",
 				PO_NavView.getP().getString("nav.message.users", PO_Properties.getSPANISH()));
 		// Pinchamos en la opción de menú de invitaciones
-		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
+		List<WebElement> elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de invitaciones.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
 		elementos.get(0).click();
 		// Aceptamos una de las dos invitaciones y comprobamos que solo queda una
-		elementos = PO_View.checkElement(driver, "free", "//td/div");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//td/div");
 		assertEquals(2, elementos.size());
 		elementos.get(0).findElement(By.className("btn")).click();
 		driver.navigate().refresh();
-		elementos = PO_View.checkElement(driver, "free", "//td/div");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//td/div");
 		assertEquals(1, elementos.size());
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
@@ -410,13 +410,13 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		PO_NavView.checkElement(driver, "text",
 				PO_NavView.getP().getString("nav.message.users", PO_Properties.getSPANISH()));
 		// Pinchamos en la opción de menú de amigos
-		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'friends-menu')]/a");
+		List<WebElement> elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'friends-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de amigos.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/friend/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/friend/list')]");
 		elementos.get(0).click();
 		// Miramos que tenemos un amigo
-		elementos = PO_View.checkElement(driver, "free", "//td[contains(text(), 'jaimitosdi@uniovi.es')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//td[contains(text(), 'jaimitosdi@uniovi.es')]");
 		assertEquals(1, elementos.size());
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
@@ -498,9 +498,9 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Accedemos al listado de mis publicaciones
 		driver.get(URL+"/publication/list");
 		// Comprobamos que entramos en la pagina publicaciones
-		PO_NavView.checkKey(driver,"publications.title", PO_Properties.getSPANISH());
+		PO_PrivateView.checkKey(driver,"publications.title", PO_Properties.getSPANISH());
 		// Comprobamos que esta la publicacion
-		PO_View.checkElement(driver, "free",
+		PO_PrivateView.checkElement(driver, "free",
 				"/html/body/div[1]/div[1]/table/tbody/tr[td[contains(text(), 'Primera Publicacion')]]");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
@@ -572,14 +572,14 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		// Navegamos hasta la opcion de menu de administrador
 		driver.get(URL+"/user/all");
 		// Comprobamos que aparecen usurios normales
-		PO_View.checkElement(driver, "text", "max@arcadia.com");
-		PO_View.checkElement(driver, "text", "pepitosdi@uniovi.es");
-		PO_View.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
-		PO_View.checkElement(driver, "text", "jaimitosdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "max@arcadia.com");
+		PO_PrivateView.checkElement(driver, "text", "pepitosdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "laurapalmer@twinpeaks.com");
+		PO_PrivateView.checkElement(driver, "text", "jaimitosdi@uniovi.es");
 		// Comprobamos que sale el otro administrador
-		PO_View.checkElement(driver, "text", "juansdi@uniovi.es");
+		PO_PrivateView.checkElement(driver, "text", "juansdi@uniovi.es");
 		// Comprobamos que no sale el propio usuario logueado
-		PO_View.checkNoElement(driver, "legoshi@beastars.es");
+		PO_PrivateView.checkNoElement(driver, "legoshi@beastars.es");
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
 	}
@@ -599,22 +599,22 @@ public class Sdi1920Entrega1411408ApplicationTests {
 		List<WebElement> elementos;
 
 		// Pinchamos en la opción de menú de amigos
-		elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'friends-menu')]/a");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'friends-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de amigos.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/friend/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/friend/list')]");
 		elementos.get(0).click();
 		// Miramos que estamos en la vista de amigos
-		PO_View.checkKey(driver, "friends.title", language);
+		PO_PrivateView.checkKey(driver, "friends.title", language);
 
 		// Pinchamos en la opción de menu de invitaciones
-		elementos = PO_View.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//li[contains(@id, 'invitations-menu')]/a");
 		elementos.get(0).click();
 		// Pinchamos en la opción de lista de amigos.
-		elementos = PO_View.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
+		elementos = PO_PrivateView.checkElement(driver, "free", "//a[contains(@href, '/invitation/list')]");
 		elementos.get(0).click();
 		// Miramos que estamos en la vista de amigos
-		PO_View.checkKey(driver, "invitations.title", language);
+		PO_PrivateView.checkKey(driver, "invitations.title", language);
 
 		// Nos desconectamos
 		PO_PrivateView.logout(driver);
